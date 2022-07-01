249e9982204de1381f8199044199f2c851aff909
martin0075 <83312013+martin0075@users.noreply.github.com>
2022-07-01 08:41:16 +0200

    home

---
+++ swengfinal/src/com/swengfinal/project/client/Auth.java
@@ -0,0 +1,30 @@
+package com.swengfinal.project.client;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Console;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Window;
+
+
+public class Auth implements EntryPoint{
+	
+	public void onModuleLoad() {
+		final Button btnHome=new Button("Send");
+		RootPanel.get("Login").add(btnHome);
+		
+		
+		
+		
+		
+    
+        
+        
+	}
+
+}
